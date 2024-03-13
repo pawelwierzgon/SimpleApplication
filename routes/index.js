398d43388ef64d077d6da5fb6483de1d845e3ee5
Paweł Wierzgoń <pawel.wierzgon@gmail.com>
2024-03-13 21:57:18 +0000

    Greeting test

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var utils = require("../modules/utils");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   let greeting = utils.sayHello(new Date(Date.now()).getHours());
-  res.render("index", { title: "Simple Application", greeting: greeting });
+  res.render("index", { title: "Simple Application", greeting: "test" });
 });
 
 module.exports = router;
